5c91b85e11dd281bb0dfe58aa627fa2e4d3b1231
khemiriahmed <khemiriahmed6@gmail.com>
2026-06-14 08:50:00 +0100

    Services

---
+++ src/main/java/com/akh/SpringBootUsingJwt/services/AppUserService.java
@@ -0,0 +1,33 @@
+package com.akh.SpringBootUsingJwt.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.akh.SpringBootUsingJwt.models.AppUser;
+import com.akh.SpringBootUsingJwt.repositories.AppUserRepository;
+
+@Service
+public class AppUserService implements UserDetailsService {
+
+    @Autowired
+    private AppUserRepository repo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        AppUser appUser = repo.findByUsername(username);
+
+        if (appUser != null) {
+            var springUser = User.withUsername(appUser.getUsername())
+                    .password(appUser.getPassword())
+                    .roles(appUser.getRole())
+                    .build();
+            return springUser;
+        }
+
+        throw new UsernameNotFoundException("User not found: " + username);
+    }
+}

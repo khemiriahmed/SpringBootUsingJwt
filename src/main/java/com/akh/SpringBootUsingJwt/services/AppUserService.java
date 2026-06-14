8e658d60eae421e9c86534304a2235f81f0536e2
khemiriahmed <khemiriahmed6@gmail.com>
2026-06-14 11:00:29 +0100

    AppUsrService

--- src/main/java/com/akh/SpringBootUsingJwt/services/AppUserService.java
+++ src/main/java/com/akh/SpringBootUsingJwt/services/AppUserService.java
@@ -1,5 +1,6 @@
 package com.akh.SpringBootUsingJwt.services;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -7,27 +8,28 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+
 import com.akh.SpringBootUsingJwt.models.AppUser;
 import com.akh.SpringBootUsingJwt.repositories.AppUserRepository;
 
 @Service
-public class AppUserService implements UserDetailsService {
+public class AppUserService implements UserDetailsService{
+	
+@Autowired
+private AppUserRepository repo;
+@Override
 
-    @Autowired
-    private AppUserRepository repo;
+public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        AppUser appUser = repo.findByUsername(username);
-
-        if (appUser != null) {
-            var springUser = User.withUsername(appUser.getUsername())
-                    .password(appUser.getPassword())
-                    .roles(appUser.getRole())
-                    .build();
-            return springUser;
-        }
-
-        throw new UsernameNotFoundException("User not found: " + username);
-    }
+	AppUser appUser = repo.findByUsername(username);
+	
+	if( appUser != null) {
+		var springUser = User.withUsername(appUser.getUsername())
+				         .password(appUser.getPassword())
+ 	                     .roles(appUser.getRole())
+ 	                     .build();
+		return springUser;
+	}
+	return null;
+}
 }

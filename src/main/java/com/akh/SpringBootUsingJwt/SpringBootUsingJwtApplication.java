e7a0c2822708ee5d956cc7fff74610c23bb69348
khemiriahmed <khemiriahmed6@gmail.com>
2026-06-14 06:41:16 +0100

    Spring boot Using Jwt

---
+++ src/main/java/com/akh/SpringBootUsingJwt/SpringBootUsingJwtApplication.java
@@ -0,0 +1,13 @@
+package com.akh.SpringBootUsingJwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUsingJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUsingJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akh/SpringBootUsingJwt/SpringBootUsingJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akh.SpringBootUsingJwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUsingJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
